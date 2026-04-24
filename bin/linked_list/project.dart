c917ba04161ad3fdf6b4f231a765d3c663f4c7b2
viniciusdacunhaferreira <viniciusdacunha.ferreira@gmail.com>
2026-04-24 20:44:47 -0300

    Implement linked list Node class with toString method

---
+++ bin/linked_list/project.dart
@@ -0,0 +1,12 @@
+import 'package:dsad/linked_list.dart';
+
+void main() {
+  final node1 = Node(value: 1);
+  final node2 = Node(value: 2);
+  final node3 = Node(value: 3);
+
+  node1.next = node2;
+  node2.next = node3;
+
+  print(node1);
+}
