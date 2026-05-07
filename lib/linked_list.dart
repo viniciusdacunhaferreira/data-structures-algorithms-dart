20dd636b506ef31c358a02cf8321b90d3d363956
viniciusdacunhaferreira <viniciusdacunha.ferreira@gmail.com>
2026-05-07 10:25:35 -0300

    Implement LinkedList iterator and extend Iterable

--- lib/linked_list.dart
+++ lib/linked_list.dart
@@ -11,10 +11,11 @@ class Node<T> {
   }
 }
 
-class LinkedList<E> {
+class LinkedList<E> extends Iterable<E> {
   Node<E>? head;
   Node<E>? tail;
 
+  @override
   bool get isEmpty => head == null;
 
   void push(E value) {
@@ -96,5 +97,33 @@ class LinkedList<E> {
   String toString() {
     if (isEmpty) return 'Empty list';
     return head.toString();
+  }
+
+  @override
+  Iterator<E> get iterator => _LinkedListIterator(this);
+}
+
+class _LinkedListIterator<E> implements Iterator<E> {
+  _LinkedListIterator(LinkedList<E> list) : _list = list;
+
+  final LinkedList<E> _list;
+  Node<E>? _currentNode;
+  bool _firstPass = true;
+
+  @override
+  E get current => _currentNode!.value;
+
+  @override
+  bool moveNext() {
+    if (_list.isEmpty) return false;
+
+    if (_firstPass) {
+      _currentNode = _list.head;
+      _firstPass = false;
+    } else {
+      _currentNode = _currentNode?.next;
+    }
+
+    return _currentNode != null;
   }
 }
