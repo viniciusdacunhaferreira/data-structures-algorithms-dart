3313b2242bccf25877522a993a2eff6db961cc09
viniciusdacunhaferreira <viniciusdacunha.ferreira@gmail.com>
2026-05-07 11:38:20 -0300

    Add LinkedList project examples

--- bin/linked_list/project.dart
+++ bin/linked_list/project.dart
@@ -12,4 +12,53 @@ void main() {
   list.insertAfter(middleNode, 42);
 
   print('After: $list');
+
+  print('\nPopping from the front of a list');
+  final list2 = LinkedList<String>()
+    ..append('a')
+    ..append('b')
+    ..append('c');
+
+  print('Before: $list2');
+
+  final poppedValue = list2.pop();
+
+  print('Popped value: $poppedValue');
+  print('After: $list2');
+
+  print('\nRemoving from the end of a list');
+  final list3 = LinkedList<String>()
+    ..append('a')
+    ..append('b')
+    ..append('c');
+
+  print('Before: $list3');
+
+  final removedValue = list3.removeLast();
+
+  print('Removed value: $removedValue');
+  print('After: $list3');
+
+  print('\nRemoving from the middle of a list');
+  final list4 = LinkedList<String>()
+    ..append('a')
+    ..append('b')
+    ..append('c');
+
+  print('Before: $list4');
+
+  final removedValue4 = list4.removeAfter(list4.nodeAt(0)!);
+
+  print('Removed value: $removedValue4');
+  print('After: $list4');
+
+  print('\nMaking a List Iterable');
+  final list5 = LinkedList<String>()
+    ..append('a')
+    ..append('b')
+    ..append('c');
+
+  for (final e in list5) {
+    print(e);
+  }
 }
